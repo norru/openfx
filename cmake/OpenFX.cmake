464480353d36f94a00e4d7f5c5bc1cfa3d30bd7c
Ole-André Rodlie <ole-andre.rodlie@inria.fr>
2016-11-08 11:45:47 +0100

    cmake fixes

--- cmake/OpenFX.cmake
+++ cmake/OpenFX.cmake
@@ -1,16 +1,24 @@
-
 # Get architecture as defined by the OFX standard
 function(get_ofx_architecture OFX_ARCH)
-    # Win
-    if(MINGW)
-        set(OFX_ARCH "Win32" PARENT_SCOPE)
+    # Arch
+    if(NOT BITS)
         if(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "x86_64")
+            set(BITS "64")
+        else()
+            set(BITS "32")
+        endif()
+    endif()
+
+    # Windows
+    if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+        set(OFX_ARCH "Win32" PARENT_SCOPE)
+        if(${BITS} STREQUAL "64")
             set(OFX_ARCH "Win64" PARENT_SCOPE)
         endif()
     # FreeBSD
     elseif(${CMAKE_SYSTEM_NAME} STREQUAL "FreeBSD")
         set(OFX_ARCH "FreeBSD-x86" PARENT_SCOPE)
-        if(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "x86_64")
+        if(${BITS} STREQUAL "64")
             set(OFX_ARCH "FreeBSD-x86-64" PARENT_SCOPE)
         endif()
     # MacOS
@@ -19,12 +27,11 @@ function(get_ofx_architecture OFX_ARCH)
     # Linux
     else()
         set(OFX_ARCH "Linux-x86" PARENT_SCOPE)
-        if(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "x86_64")
+        if(${BITS} STREQUAL "64")
             set(OFX_ARCH "Linux-x86-64" PARENT_SCOPE)
         endif()
     endif()
 endfunction(get_ofx_architecture) 
-
 
 # Create a new OFX plugin
 function(create_ofx_plugin PLUGIN_NAME PLUGIN_SRC PLUGIN_LDFLAGS PLUGIN_RESOURCES PLUGIN_INCLUDES PLUGIN_EXTRA_SOURCES)
@@ -38,8 +45,11 @@ function(create_ofx_plugin PLUGIN_NAME PLUGIN_SRC PLUGIN_LDFLAGS PLUGIN_RESOURCE
 
     include_directories(${PLUGIN_INCLUDES})
 
+    get_ofx_architecture(OFX_ARCH)
+
     # Plugin target is a shared library
     add_library(${PLUGIN_NAME} MODULE ${PLUGIN_SOURCES} ${PLUGIN_EXTRA_SOURCES})
+    set_target_properties(${PLUGIN_NAME} PROPERTIES COMPILE_FLAGS "-m${BITS}" LINK_FLAGS "-m${BITS}")
 
     # Link with OfxSupport library
     target_link_libraries(${PLUGIN_NAME} OfxSupport)
@@ -58,25 +68,26 @@ function(create_ofx_plugin PLUGIN_NAME PLUGIN_SRC PLUGIN_LDFLAGS PLUGIN_RESOURCE
         set_target_properties(${PLUGIN_NAME} PROPERTIES LINK_FLAGS "-bundle -fvisibility=hidden -exported_symbols_list,${OFX_SUPPORT_HEADER_DIR}/osxSymbols")
         set_target_properties(${PLUGIN_NAME} PROPERTIES INSTALL_RPATH "@loader_path/../Frameworks;@loader_path/../Libraries")
     elseif(WIN32)
-        set_target_properties(${PLUGIN_NAME} PROPERTIES LINK_FLAGS "-fvisibility=hidden,--version-script=${OFX_SUPPORT_HEADER_DIR}/linuxSymbols")
+        if(MINGW)
+           add_definitions(-U__STRICT_ANSI__ -D__USE_MINGW_ANSI_STDIO=1)
+        endif()
+        set_target_properties(${PLUGIN_NAME} PROPERTIES LINK_FLAGS "-shared -fvisibility=hidden -Xlinker --version-script=${OFX_SUPPORT_HEADER_DIR}/linuxSymbols")
     else()
-        set_target_properties(${PLUGIN_NAME} PROPERTIES LINK_FLAGS "-Wl,-fvisibility=hidden,--version-script=${OFX_SUPPORT_HEADER_DIR}/linuxSymbols")
+        set_target_properties(${PLUGIN_NAME} PROPERTIES LINK_FLAGS "-shared -fvisibility=hidden -Xlinker --version-script=${OFX_SUPPORT_HEADER_DIR}/linuxSymbols")
         set_target_properties(${PLUGIN_NAME} PROPERTIES INSTALL_RPATH "$ORIGIN/../../Libraries")
     endif()
 
     # Install OFX plugin
-    get_ofx_architecture(OFX_ARCH)
     install(TARGETS ${PLUGIN_NAME}
             DESTINATION "${CMAKE_INSTALL_PREFIX}/OFX/Plugins/${PLUGIN_NAME}.ofx.bundle/Contents/${OFX_ARCH}"
             COMPONENT OfxPlugins
             OPTIONAL)
-    if (EXISTS "${PROJECT_SOURCE_DIR}/${PLUGIN_SRC}/Info.plist")
-        install(FILES "${PROJECT_SOURCE_DIR}/${PLUGIN_SRC}/Info.plist"
+    if (EXISTS "${PROJECT_SOURCE_DIR}/${PLUGIN_NAME}/Info.plist")
+        install(FILES "${PROJECT_SOURCE_DIR}/${PLUGIN_NAME}/Info.plist"
                 DESTINATION "${CMAKE_INSTALL_PREFIX}/OFX/Plugins/${PLUGIN_NAME}.ofx.bundle/Contents")
     endif()
     if (PLUGIN_RESOURCES)
         install(FILES ${PLUGIN_RESOURCES}
                 DESTINATION "${CMAKE_INSTALL_PREFIX}/OFX/Plugins/${PLUGIN_NAME}.ofx.bundle/Contents/Resources")
     endif()
-
 endfunction(create_ofx_plugin)
