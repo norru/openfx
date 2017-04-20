bab37d7dc1be353c0f803fba60305cb7405ccc40
Frédéric Devernay <frederic.devernay@m4x.org>
2017-04-20 09:58:24 +0200

    Revert "ImageEffectDescriptor::addClipPreferencesSlaveParam: slave params never animate: force it"
    This reverts commit 3b5dbb21de82dcf9b2f02712d3ebd3609651e8da.

--- Support/Library/ofxsImageEffect.cpp
+++ Support/Library/ofxsImageEffect.cpp
@@ -1346,10 +1346,13 @@ namespace OFX {
   /** @brief If the slave param changes, the clip preferences need to be re-evaluated. Also disables param animation. */
   void ImageEffectDescriptor::addClipPreferencesSlaveParam(ParamDescriptor &p)
   {
-    // ClipPreferencesSlaveParam cannot be animated
-    p.getPropertySet().propSetInt(kOfxParamPropAnimates, false);
     int n = _effectProps.propGetDimension(kOfxImageEffectPropClipPreferencesSlaveParam);
     validateXMLString(p.getName(), false);
+# ifdef DEBUG
+    if (p.getPropertySet().propGetInt(kOfxParamPropAnimates)) {
+      std::cout << "Warning: parameter " << p.getName() << " is a clip preferences slave param but is animated\n";
+    }
+# endif
     _effectProps.propSetString(kOfxImageEffectPropClipPreferencesSlaveParam, p.getName(), n);
   }
 
