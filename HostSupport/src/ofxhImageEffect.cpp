44ea8776a7de3395fa1e052d0f0dcaaa2f3582fe
Frédéric Devernay <frederic.devernay@m4x.org>
2017-04-26 18:24:08 +0200

    HostSupport: set reasonable default value on outArgs for backward compatibility

--- HostSupport/src/ofxhImageEffect.cpp
+++ HostSupport/src/ofxhImageEffect.cpp
@@ -2439,6 +2439,10 @@ namespace OFX {
 #endif
           
         Property::Set outArgs(outStuff);
+#ifdef OFX_EXTENSIONS_NUKE
+        // set the default value on outArgs for backward compatibility
+        outArgs.setIntProperty(kFnOfxImageEffectPropView, view);
+#endif
 
 #       ifdef OFX_DEBUG_ACTIONS
           OfxPlugin *ofxp = _plugin->getPluginHandle()->getOfxPlugin();
