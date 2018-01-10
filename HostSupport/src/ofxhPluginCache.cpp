3ca381a504af13837e3957fa68e08cab5df9470a
Frédéric Devernay <frederic.devernay@m4x.org>
2018-01-10 12:30:10 +0100

    fix travis build

--- HostSupport/src/ofxhPluginCache.cpp
+++ HostSupport/src/ofxhPluginCache.cpp
@@ -268,8 +268,14 @@ PluginCache::~PluginCache()
   _binaries.clear();
 }
 
-PluginCache::PluginCache() : _hostSpec(NULL), _staticBinary(NULL), _xmlCurrentBinary(NULL), _xmlCurrentPlugin(NULL) {
-  
+PluginCache::PluginCache()
+: _hostSpec(NULL)
+#ifdef OFX_USE_STATIC_PLUGINS
+, _staticBinary(NULL)
+#endif
+, _xmlCurrentBinary(NULL)
+, _xmlCurrentPlugin(NULL)
+{
   _cacheVersion = "";
   _ignoreCache = false;
   _dirty = false;
