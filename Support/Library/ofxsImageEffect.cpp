2a019f59c233a2f8bef40ecad07473f2cc637805
Frédéric Devernay <frederic.devernay@m4x.org>
2018-02-15 14:00:17 +0100

    Support: fix wrong ifdef linux

--- Support/Library/ofxsImageEffect.cpp
+++ Support/Library/ofxsImageEffect.cpp
@@ -56,7 +56,7 @@ England
 #endif
 #include "ofxsCore.h"
 
-#if defined __APPLE__ || defined linux || defined __FreeBSD__
+#if defined __APPLE__ || defined __linux__ || defined __FreeBSD__
 # if __GNUC__ >= 4
 #  define EXPORT __attribute__((visibility("default")))
 #  define LOCAL  __attribute__((visibility("hidden")))
